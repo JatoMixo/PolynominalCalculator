10728c1de84c35005b504fce89938029b7b80f6e
JatoMixo <jatomixo@outlook.es>
2023-01-13 20:03:35 +0100

    main

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,18 @@
 #include <iostream>
+#include <map>
 #include <D:/Miguel/Programas/PolynominalCalculator/src/polynominal.cpp>
 
 int main(){
 
-  
+  // Hashmap to store declared polynominals
+  // =Example=
+  // "P(x)", Polynominal(3, 4x^2, etc)
+  std::map<std::string, Polynominal> polynominals;
+
+  // Infinite loop
+  while (true){
+    std::cout << "\n>>> ";
+  }
+
   return 0;
 }
--- src/polynominal.cpp
+++ src/polynominal.cpp
@@ -4,6 +4,9 @@
 
 class Polynominal{
 
+  public:
+  Polynominal(){}
+
   // Crete a map to store the coefficients of the polynominals for each variable.
   // Example: {"x", {1, 0, 5, -7}} => -7x^4 + 5x^3 + x + rest
   // Example with 2 variables: {"x", {1, 2}}, {"y", {0, 3, 4}} => 4y^3 + 3*2x^2y^2 + x + rest
