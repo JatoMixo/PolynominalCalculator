3bc663b7b86e74db03f23793d409cf329fdc2441
JatoMixo <jatomixo@outlook.es>
2023-01-14 20:41:00 +0100

    main

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
 #include <map>
+#include <algorithm>
 #include </home/jatomixo/Code/PolynominalCalculator/src/polynominal.cpp>
+
+std::string polynominalToString(Polynominal pol);
+Polynominal SumPolynominals(Polynominal first, Polynominal second);
+Polynominal RestPolynominals(Polynominal first, Polynominal second);
+Polynominal MultiplyPolynominals(Polynominal first, Polynominal second);
+Polynominal DividePolynominals(Polynominal first, Polynominal second);
 
 int main(){
 
@@ -12,6 +19,28 @@ int main(){
   // Infinite loop
   while (true){
     std::cout << "\n>>> ";
+
+    std::string firstVarName;
+    char operation;
+    std::string secondVarName;
+
+    std::cin >> firstVarName >> operation >> secondVarName;
+
+    if (operation == '='){
+
+    } else if (!polynominals.find(firstVarName) && !polynominals.find(secondVarName)) {
+      std::cout << "Invalid polynominal => Doesn't exist.\n";
+    }
+    
+    if (operation == '+'){
+      std::cout << polynominalToString(SumPolynominals(polynominals[firstVarName], polynominals[secondVarName]));
+    } else if (operation == '-'){
+      std::cout << polynominalToString(RestPolynominals(polynominals[firstVarName], polynominals[secondVarName]));
+    } else if (operation == '*'){
+      std::cout << polynominalToString(MultiplyPolynominals(polynominals[firstVarName], polynominals[secondVarName]));
+    } else if (operation == '/'){
+      std::cout << polynominalToString(DividePolynominals(polynominals[firstVarName], polynominals[secondVarName]));
+    }
   }
 
   return 0;
--- src/polynominal.cpp
+++ src/polynominal.cpp
@@ -13,6 +13,4 @@ class Polynominal{
   // Create a map to store the literal parts of each variable in the polynominal.
   // Example: {"X", {0, 1, 1}}, {"y", {1}} => x^3 + x^2 + y
   std::map<std::string, std::vector<int>> literalParts;
-
-  int rest;
 };
