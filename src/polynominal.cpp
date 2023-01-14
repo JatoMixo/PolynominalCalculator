e3f95e6c8f1768261d4d501dc9fa2e6b3b36a59b
JatoMixo <jatomixo@outlook.es>
2023-01-14 15:42:02 +0100

    main

--- src/polynominal.cpp
+++ src/polynominal.cpp
@@ -11,7 +11,7 @@ class Polynominal{
   std::vector<int> coefficients;
 
   // Create a map to store the literal parts of each variable in the polynominal.
-  // Example: {"X", {0, 1, 3}}, {"y", {1}} => x^3 + x^2 + y
+  // Example: {"X", {0, 1, 1}}, {"y", {1}} => x^3 + x^2 + y
   std::map<std::string, std::vector<int>> literalParts;
 
   int rest;
