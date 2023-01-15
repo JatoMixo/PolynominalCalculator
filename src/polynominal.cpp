7302a2feb458cff4025e605501e20e5345fca984
JatoMixo <jatomixo@outlook.es>
2023-01-15 19:00:24 +0100

    Added new constructor to monomial

--- src/polynominal.cpp
+++ src/polynominal.cpp
@@ -8,6 +8,7 @@ class Monomial{
   public:
 
   Monomial() {}
+  Monomial(int coefficient, std::map<std::string, int> literalPart) {this->coefficient = coefficient; this->literalPart = literalPart;}
 
   int coefficient;
   std::map<std::string, int> literalPart;
